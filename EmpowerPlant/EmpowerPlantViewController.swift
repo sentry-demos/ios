1b7e61fbfa928942173522ac44bd8c8101e60174
William Capozzoli <thinkocapo@gmail.com>
2022-03-08 17:23:09 -0800

    first commit

---
+++ EmpowerPlant/EmpowerPlantViewController.swift
@@ -0,0 +1,57 @@
+//
+//  EmpowerPlantViewController.swift
+//  EmpowerPlant
+//
+//  Created by William Capozzoli on 3/8/22.
+//
+
+import UIKit
+
+class EmpowerPlantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Empower Plant"
+        // Do any additional setup after loading the view.
+        configureNavigationItems()
+    }
+    
+    private func configureNavigationItems() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Cart",
+            style: .plain,
+            target: self,
+            action: #selector(goToCart)
+        )
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "List App",
+            style: .plain,
+            target: self,
+            action: #selector(goToListApp)
+        )
+    }
+    
+    @objc
+    func goToCart() {
+        print("go to Cart")
+        self.performSegue(withIdentifier: "goToCart", sender: self)
+    }
+    
+    @objc
+    func goToListApp() {
+        print("go to List App")
+        self.performSegue(withIdentifier: "goToListApp", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+
+## Setup
+iPhone 11, as X%
+
+## Run
+
+
+
+
+## Bootstrapping iOS App
+Followed for ViewController -> Embed In -> Navigation Controller, and Basic Styling
+https://www.youtube.com/watch?v=LbAd2FIlnos
+
+Segue's
+1. CMD+Click EmpowerPlantViewController icon (1st of 3) on Storyboard, drag to next ViewController
+2. Click the new segueu line arrow -> Storyboard Segue options -> Identifier, set as goToCart
+
+
+Storyboard > ViewController > Navigation Item > BackButton renaming does not work
+
+
+Dragged 'Checkout' button to bottom but it displays in middle in the virtual device, maybe because added constraints for Horizontal/Vertical Orientation.
