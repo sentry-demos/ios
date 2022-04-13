4baaaf1189d1f9828e4cbfc8a7c7051fc060b217
William Capozzoli <thinkocapo@gmail.com>
2022-04-13 13:09:03 +0200

    Quantities class on ShoppingCart, testing

--- EmpowerPlant/ShoppingCart.swift
+++ EmpowerPlant/ShoppingCart.swift
@@ -25,22 +25,72 @@ class ShoppingCart {
     // }
     
     var items = [Product]()
-    var quantities = {}
     var price = 0
+    // var quantities = {}
+    // class quantities: NSObject {}
+    // var quantities: NSObject = {}
+    // struct quantities {}
+    // var quantities = {
+    //    "1": 0;
+    //    "2": 0;
+    // }
+    // struct quantities {
+    //    var one = 0
+    //    var two = 0
+    // }
+    
+    // var city:City!
+    var quantities = Quantities()
     
     // https://github.com/sentry-demos/application-monitoring/blob/master/react/src/reducers/index.js#L23-L32
     static func addProduct(product: Product) {
-        print("addProduct", product)
+        // print("addProduct", product)
+        // print("isEmpty", self.instance.isEmpty)
+        
+        if self.instance.items.contains(product) {
+            print("has it", product.productId!)
+        } else {
+            print("doesn't have it")
+            self.instance.items.append(product)
+        }
+        
+        self.instance.quantities.name = self.instance.quantities.name + 1
+        print("quantities.name", self.instance.quantities.name)
+        
+        // self.instance.quantities.setValue(0, forKey: "key")
+        // self.instance.quantities['prop1'] = 0
+        // self.instance.quantities[product.productId] = self.instance.quantities[product.productId] || 0
+        // self.instance.quantities["prop1"] = 0 // struct
+        // https://stackoverflow.com/questions/31145990/dynamically-create-objects-and-set-attributes-in-swift
+        // https://stackoverflow.com/questions/38121957/singleton-and-class-properties-in-swift
+        
         /*
          var cart = Object.assign({}, state.cart)
+         DONE
            let item = cart.items.find((x) => x.id === payload.product.id);
            if (!item) cart.items.push(payload.product);
+         NEXT
            cart.quantities[payload.product.id] = cart.quantities[payload.product.id] || 0;
            cart.quantities[payload.product.id]++;
+         
            cart.total = cart.items.reduce((a, item) => {
              const itemTotal = item.price * cart.quantities[item.id];
              return a + itemTotal;
            }, 0);
          */
+    }
+}
+
+// TODO can setKey on this?
+class Quantities: NSObject {
+
+    var _name:Int = 0
+    var name:Int {
+        get {
+            return _name
+        }
+        set (newVal) {
+            _name = newVal
+        }
     }
 }
