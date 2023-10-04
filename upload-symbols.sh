7535bd5dc89ec1ede15c1e63e1bf6c393c23ac4b
Andrew McKnight <andrew.mcknight@sentry.io>
2023-10-04 11:14:11 -0700

    dont blackhole sentry-cli stderr output

--- upload-symbols.sh
+++ upload-symbols.sh
@@ -22,7 +22,7 @@ if which sentry-cli >/dev/null; then
         exit 1
     fi
 
-    ERROR=$(sentry-cli upload-dif --force-foreground --include-sources "$DWARF_DSYM_FOLDER_PATH" 2>&1 >/dev/null)
+    ERROR=$(sentry-cli upload-dif --force-foreground --include-sources "$DWARF_DSYM_FOLDER_PATH")
     if [ ! $? -eq 0 ]; then
         echo "warning: sentry-cli - $ERROR"
     fi
