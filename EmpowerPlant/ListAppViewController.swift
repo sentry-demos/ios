3576aac05d440884d6c1b7ec31b2e294a60c5909
Andrew McKnight <amcknight2718@gmail.com>
2023-10-12 15:30:38 -0800

    feat: Regex on Main Thread profiling issue (#61)

--- EmpowerPlant/ListAppViewController.swift
+++ EmpowerPlant/ListAppViewController.swift
@@ -277,4 +277,12 @@ class ListAppViewController: UIViewController {
             }
         }
     }
+
+    @IBAction func regexOnMainThread(_ sender: Any) {
+        let string = try! String(contentsOf: Bundle.main.url(forResource: "mobydick", withExtension: "txt")!)
+        let regex = try! NSRegularExpression(pattern: "([Tt]he)?.*([Ww]hale)")
+        let span = SentrySDK.startTransaction(name: "test", operation: "regex-on-main")
+        regex.matches(in: string, range: NSMakeRange(0, string.count))
+        span.finish()
+    }
 }
